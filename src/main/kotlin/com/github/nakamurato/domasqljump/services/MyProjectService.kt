48f8da44d595f8394a6f53012e699af30483cb89
GitHub Action <action@github.com>
2024-11-23 01:12:07 +0000

    Template cleanup

--- src/main/kotlin/com/github/nakamurato/domasqljump/services/MyProjectService.kt
+++ src/main/kotlin/com/github/nakamurato/domasqljump/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.nakamurato.domasqljump.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.nakamurato.domasqljump.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
